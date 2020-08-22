c6643907068fa1aedcab91122d47ce5a797df4d9
Ebenezer <ebene2009@gmail.com>
2020-08-22 20:30:08 +0100

    Added decrement function

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,48 +1,69 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-  state = {
-    count: 0,
-    tags: ["tag1", "tag2", "tag3"],
-  };
+  // state = {
+  //   value: this.props.counter.value,
+  // };
 
   //   constructor() {
   //     super();
   //     this.handingIncrement = this.handingIncrement.bind(this);
   //   }
 
-  handingIncrement = product => {
-    // console.log(this);
-    // this.state.count++
-    console.log(product);
-    this.setState({ count: this.state.count + 1 });
-    // console.log(this.state.count, this);
-  };
+  // handleIncrement = () => {
+  //   this.setState({ value: this.state.value + 1 });
+  //   // console.log(this.state.count, this);
+  // };
 
   render() {
+    const { doIncrement, decrement, counter, onDelete } = this.props;
     return (
-      <React.Fragment>
-        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button
-          onClick={() => this.handingIncrement({id:1})}
-          className="btn btn-secondary btn-sm"
-        >
-          Increment
-        </button>
-      </React.Fragment>
+      <div className="row">
+        <div className="col-1">
+          <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        </div>
+        <div className="col">
+          <button
+            onClick={() => doIncrement(counter)}
+            className="btn btn-secondary btn-sm"
+          >
+            +
+          </button>
+          <button
+            disabled= {this.disableButton()}
+            onClick={() => decrement(counter)}
+            className="btn btn-secondary btn-sm m-2"
+          >
+            -
+          </button>
+          <button
+            onClick={() => onDelete(counter.id)}
+            className="btn btn-danger btn-sm"
+          >
+            X
+          </button>
+        </div>
+      </div>
     );
   }
 
   getBadgeClasses() {
-    let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    let classes = "badge badge-";
+    classes += this.props.counter.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    const { count } = this.state;
-    return count === 0 ? "Zero" : count;
+    const { value } = this.props.counter;
+    return value === 0 ? "Zero" : value;
   }
+
+  disableButton(){
+    const { value } = this.props.counter;
+    return value === 0 ? "disabled" : "";
+
+  }
+
 }
 
 export default Counter;
---
+++ src/components/navbar.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const NavBar = ({totalCounters}) => {
+   
+    return (
+      <nav className="navbar navbar-light bg-light">
+        <a href="#" className="navbar-brand">
+          Navbar{" "}
+          <span className="badge badge-pill badge-secondary">
+            {totalCounters}
+          </span>
+        </a>
+      </nav>
+    );
+}
+ 
+export default NavBar;
+
+// class NavBar extends Component {
+//   render() {
+//   }
+// }
+
+// export default NavBar;
